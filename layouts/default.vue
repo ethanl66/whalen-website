840463f99dc04e1074a38969838858e20f72c17b
michael whalen <michael.whalen@sitechhs.com>
2021-02-10 13:36:36 -0500

    init

---
+++ layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <Nuxt />
+  </div>
+</template>
+
+<style>
+html {
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+</style>
