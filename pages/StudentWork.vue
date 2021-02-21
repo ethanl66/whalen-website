a4944f485775177db146852bb3b13488e87c0e23
michael whalen <michael.whalen@sitechhs.com>
2021-02-21 16:15:55 -0500

    testing

--- pages/StudentWork.vue
+++ pages/StudentWork.vue
@@ -1,11 +1,6 @@
 <template>
   <section class="student-work-container">
-    <TextLeft
-      title="Fantasy Kingdom"
-      image="https://res.cloudinary.com/dbopxlpuy/image/upload/c_scale,f_auto,w_800/v1613089479/Fantasy Kingdom/Screenshot_2021-02-21_Galgudd_lorm1c"
-      :authors="['Charley Baluja', 'Daniel Briskman']"
-    />
-    <TextRight />
+    <Full />
   </section>
 </template>
 
