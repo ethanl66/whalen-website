dda53082717c503196a2669c3120f5043ac52a4a
mr. whalen <michael.whalen@sitechhs.com>
2021-03-26 12:54:28 -0400

    theme workings

--- components/Themes/Color.vue
+++ components/Themes/Color.vue
@@ -1,7 +1,7 @@
 <template>
   <li class="theme-icon" @click="$colorMode.preference = color">
     <span class="icon"><fa :icon="icon" /> </span>
-    <span>{{ color }}</span>
+    <span class="color-name">{{ color }}</span>
   </li>
 </template>
 
@@ -16,8 +16,22 @@ export default {
 
 <style lang="scss">
 .theme-icon {
-  font-size: var(--h3);
+  font-size: var(--h2);
   color: var(--text-primary);
   list-style: none;
+  margin: 2rem auto;
+  transition: all 0.3s;
+}
+.icon,
+.color-name {
+  margin: 1rem;
+}
+.theme-icon:hover {
+  //transform: scale(1.1);
+
+  cursor: pointer;
+}
+.theme-icon:hover .color-name {
+  border-bottom: 3px solid white;
 }
 </style>
--- pages/Theme.vue
+++ pages/Theme.vue
@@ -15,4 +15,10 @@ export default {
 }
 </script>
 
-<style lang="scss"></style>
+<style lang="scss">
+.theme-switcher {
+  height: 40vh;
+  @include flex-col;
+  justify-content: center;
+}
+</style>
