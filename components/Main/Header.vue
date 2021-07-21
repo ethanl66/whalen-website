891051950341e28033c4170b390c8f44d991f6e8
mr. whalen <michael.whalen@sitechhs.com>
2021-07-21 10:24:22 -0400

    asd

--- components/Main/Header.vue
+++ components/Main/Header.vue
@@ -1,15 +1,24 @@
 <template>
   <header>
-    <div class="heading">
-      <h1>Michael Whalen</h1>
-      <h4>Full-Stack Educator</h4>
-      <h4>Staten Island Technical High School</h4>
+    <div class="header-flex">
+      <div class="header-container">
+        <div class="heading">
+          <h1>Michael Whalen</h1>
+          <h4>Full-Stack Educator</h4>
+          <h4>Staten Island Technical High School</h4>
+        </div>
+        <button class="header-btn btn">
+          <nuxt-link to="StudentWork" class="header-btn-link"
+            >See Our Work</nuxt-link
+          >
+        </button>
+      </div>
+      <img
+        src="https://res.cloudinary.com/dbopxlpuy/image/upload/c_scale,f_auto,w_1200/v1613089479/Fantasy%20Kingdom/Rotato_Snapshot1_wyqziw.png"
+        alt="model of Iphone with student work featured"
+        class="header-img"
+      />
     </div>
-    <button class="header-btn btn">
-      <nuxt-link to="StudentWork" class="header-btn-link"
-        >See Our Work</nuxt-link
-      >
-    </button>
   </header>
 </template>
 
@@ -20,9 +29,7 @@ export default {}
 <style lang="scss">
 header {
   height: 100vh;
-  background-image: url('https://res.cloudinary.com/dbopxlpuy/image/upload/b_rgb:000000,f_auto,o_19/v1611365531/Bao/test_c6zrj6.png');
-  background-size: cover;
-  background-position: center;
+  background-color: #000;
   clip-path: polygon(0 0, 100% 0%, 100% 87%, 0% 100%);
   h1 {
     text-transform: uppercase;
@@ -34,21 +41,20 @@ header {
     background-image: url('https://res.cloudinary.com/dbopxlpuy/image/upload/b_rgb:000000,c_scale,f_auto,o_19,w_1920/v1611365531/Bao/test_c6zrj6.png');
   }
 }
-
+.header-flex {
+  @include flex-row;
+  justify-content: space-around;
+  width: 90%;
+}
+.header-img {
+  width: 30%;
+}
 .heading {
-  position: absolute;
-  top: 40%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   text-align: center;
   z-index: 2;
   color: #fff;
 }
 .header-btn {
-  position: absolute;
-  top: 80%;
-  left: 50%;
-  transform: translate(-50%, -50%);
   font-size: var(--h4);
   height: 5rem;
   color: var(--white);
